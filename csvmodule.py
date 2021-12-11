9326d69b31ff73cf15be77f77011a78d5a2975b4
Checco24fra <francescoghezze@gmail.com>
2021-12-11 12:02:17 +0100

    Csv module and dataset added

---
+++ csvmodule.py
@@ -0,0 +1,29 @@
+import csv
+
+
+def read_csv_pages(n):
+    """
+    Function to read the first 100 lines of a cdv dataset,
+    this function will print the title, the author,
+    the genre and the number of pages of all books
+    with less than n pages.
+    """
+
+    with open('books_new.csv') as csv_file:  # Opening,reading file
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        first_line = next(csv_reader)        # Gets the first line
+
+        if n == 0:                           # Conditional for input 0
+            return print('Pages must be more than one')
+        if n < 0:                            # Conditional for negative input
+            Return print('Pages can' be negative')
+        i = 0
+        for row in csv_reader:               # Iteration to get first 100 lines
+            if i < 100 and int(row[4]) < n:  # Select books with less than n pages
+
+                one = f'{row[0]} was written by '
+                two = f'{row[1]}'
+                three = f' and is {row[2]}'
+                four = f'book with {row[4]} pages'
+                print(one+two+three+four)    # Print
+                i += 1                       # Counter up 1
