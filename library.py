f94775798d1856d52ae5619074e6e72738404e11
Checco24fra <francescoghezze@gmail.com>
2021-12-12 20:35:39 +0100

    New function on library.py

--- library.py
+++ library.py
@@ -26,3 +26,33 @@ def check_author(author_name):
 
     if not found:
         print("Sorry, {} is not present.".format(author_name))
+
+
+def book_pages(number_of_p):
+
+    """
+    Function to find the book with less pages that number_of_p parameter
+    """
+
+    books_length = {'Romeo and Juliet': 322,  # New dictionary
+                    '1984': 340,
+                    '2001: a Space Odyssey': 250,
+                    'Pride and Prejudice': 450,
+                    'The Great Gatsby': 200,
+                    'The Lord of the Rings': 1100,
+                    'The Old Man and the Sea': 100,
+                    'The Picture of Dorian Gray': 300,
+                    'A Christmas Carol': 125}
+
+    max_pages = 0                               # Initialise variables
+    book = None
+    for k, v in books_length.items():            # Iteration
+        # If found new book with more pages than the
+        # previous best assign to update variable book
+        if v < number_of_p and max_pages < v:
+            max_pages = v
+            book = k
+    # Return
+    a = f'The book with most pages less than {number_of_p}'
+    b = f'is {book} with {max_pages}'
+    return a+b
